c14b838c6e4d0b106a17d2b1993813dd61171a92
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-06-29 15:07:43 +0500

    working good

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,40 @@
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp());
+void main() {
+  runApp(MaterialApp(
+    home: MyListWheelScroolView(),
+  ));
+}
+
+class MyListWheelScroolView extends StatefulWidget {
+  const MyListWheelScroolView({super.key});
+
+  @override
+  State<MyListWheelScroolView> createState() => _MyListWheelScroolViewState();
+}
+
+class _MyListWheelScroolViewState extends State<MyListWheelScroolView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Scaffold(
+          body: ListWheelScrollView.useDelegate(
+              itemExtent: 75,
+              childDelegate:
+                  ListWheelChildBuilderDelegate(builder: (context, index) {
+                if (index < 0 || index > 12) {
+                  return null;
+                }
+                return ListTile(
+                  onTap: () {
+                    
+                  },
+                  leading: CircleAvatar(
+                      child: Image.asset('images/cartoon.png')),
+                  title: Text('Person$index'),
+                  subtitle: Text('Malumot bu yerda'),
+                );
+              }))),
+    );
+  }
 }
